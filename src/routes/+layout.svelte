7a506ee33509f1242e64afcb8312ec142fbb6e32
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2025-02-17 20:58:36 +0100

    remove divider from navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,8 +8,8 @@
 </script>
 
 <nav class="bg-black text-white sticky top-0 border-white/35 border-b-1 h-15">
-	<div class="flex flex-row justify-between gap-10 items-center mx-10 *:py-4 divide-x divide-white/35 h-full">
-		<div class="pr-4">
+	<div class="flex flex-row justify-between gap-10 items-center mx-10 *:py-2 h-full">
+		<div class="pr-4 h-full flex items-center">
 			<a href="/">
 				<Logo />
 			</a>
@@ -17,7 +17,7 @@
 
 		{#if data.session?.user}
 			<div class="flex flex-row gap-4 h-full">
-				<a href="/@{data.session?.user?.handle}">
+				<a href="/@{data.session?.user?.handle}" class="h-full">
 					<img class="h-full rounded-full" src={data.session?.user?.image} alt="User" />
 				</a>
 				<button onclick={() => signOut()}>Log out</button>
