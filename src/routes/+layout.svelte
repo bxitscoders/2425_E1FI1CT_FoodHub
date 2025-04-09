86a7f43efb3c51ce7a0384237d49e67bb1342915
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2025-04-09 21:35:20 +0200

    feat: implement Navbar component for user authentication and navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,47 +1,12 @@
 <script lang="ts">
 	import "../app.css";
-	import Logo from "$lib/components/Logo.svelte";
 	import type { LayoutProps } from "./$types";
-	import { page } from "$app/state";
-	import { goto } from "$app/navigation";
-	import { authClient } from "$lib/auth-client";
+	import Navbar from "$lib/components/Navbar.svelte";
 
 	const { data, children }: LayoutProps = $props();
-
-	async function signOut() {
-		await authClient.signOut();
-		await goto("/", { invalidateAll: true });
-	}
 </script>
 
-<nav class="bg-black text-white sticky top-0 border-white/35 border-b-1 h-15">
-	<div class="flex flex-row justify-between gap-10 items-center mx-10 *:py-2 h-full">
-		<div class="pr-4 h-full flex items-center">
-			<a href="/">
-				<Logo />
-			</a>
-		</div>
-
-		{#if data.session}
-			<div class="flex flex-row gap-4 h-full">
-				<a href="/@{data.session?.user?.handle}" class="h-full">
-					<img class="h-full rounded-full" src={data.session?.user?.image} alt="User" />
-				</a>
-				<button onclick={() => signOut()}>Log out</button>
-			</div>
-		{:else}
-			<button
-				onclick={() =>
-					authClient.signIn.social({
-						provider: "github",
-						callbackURL: page.url.pathname
-					})}
-			>
-				<span class="font-medium">Log in</span>
-			</button>
-		{/if}
-	</div>
-</nav>
+<Navbar />
 
 <div class="m-2">
 	{@render children()}
