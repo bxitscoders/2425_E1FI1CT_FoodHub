1ace3ac78d71054cfa8324a1a70f0e28a6bf3f60
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2025-05-02 15:19:32 +0200

    feat(backend): integrate scalar for API documentation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.get("/", () => "Hello Elysia")
+	.use(swagger())
+	.listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
