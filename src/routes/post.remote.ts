dabb6b1cc3a97ca70252f777b39cfb21ab98afbf
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2025-11-17 14:42:44 +0100

    feat: implement createPost with minio

--- src/routes/post.remote.ts
+++ src/routes/post.remote.ts
@@ -1,133 +1,151 @@
-import { query } from "$app/server";
+import { form, getRequestEvent, query } from "$app/server";
 import { db } from "$lib/server/db";
 import { posts, postRatings } from "$lib/server/db/post-schema";
+import { MINIO_BUCKET_NAME, minioClient } from "$lib/server/minio";
+import { error } from "@sveltejs/kit";
 import { eq, sql } from "drizzle-orm";
-import * as v from 'valibot';
+import { title } from "process";
+import sharp from "sharp";
+import * as v from "valibot";
 
 export const loadPosts = query(async () => {
-    const userPosts = await db
-        .select({
-            post: posts,
-            ratingAmount: sql`COUNT(${postRatings.id})`,
-            ratingAverage: sql`AVG(${postRatings.rating})`
-        })
-        .from(posts)
-        .leftJoin(postRatings, eq(postRatings.postId, posts.id))
-        .groupBy(posts.id)
-        .orderBy(posts.createdAt)
-        .limit(10);
+	const userPosts = await db
+		.select({
+			post: posts,
+			ratingAmount: sql`COUNT(${postRatings.id})`,
+			ratingAverage: sql`AVG(${postRatings.rating})`
+		})
+		.from(posts)
+		.leftJoin(postRatings, eq(postRatings.postId, posts.id))
+		.groupBy(posts.id)
+		.orderBy(posts.createdAt)
+		.limit(10);
 
-    return userPosts.map(r => ({
-        id: r.post.id,
-        creatorUserId: r.post.userId,
-        title: r.post.title,
-        content: r.post.content,
-        createdAt: r.post.createdAt.getTime(),
-        rating: {
-            amount: Number(r.ratingAmount ?? 0),
-            average: r.ratingAverage ? Number(r.ratingAverage) : 0
-        }
-    })) as PostDTO[];
+	return userPosts.map((r) => ({
+		id: r.post.id,
+		creatorUserId: r.post.userId,
+		title: r.post.title,
+		content: r.post.content,
+		createdAt: r.post.createdAt.getTime(),
+		rating: {
+			amount: Number(r.ratingAmount ?? 0),
+			average: r.ratingAverage ? Number(r.ratingAverage) : 0
+		}
+	})) as PostDTO[];
 });
 
 export const loadPostsByOffset = query(v.object({ offset: v.number() }), async (schema) => {
-    const { offset } = schema;
-    const userPosts = await db
-        .select({
-            post: posts,
-            ratingAmount: sql`COUNT(${postRatings.id})`,
-            ratingAverage: sql`AVG(${postRatings.rating})`
-        })
-        .from(posts)
-        .leftJoin(postRatings, eq(postRatings.postId, posts.id))
-        .groupBy(posts.id)
-        .orderBy(posts.createdAt)
-        .limit(15)
-        .offset(offset);
+	const { offset } = schema;
+	const userPosts = await db
+		.select({
+			post: posts,
+			ratingAmount: sql`COUNT(${postRatings.id})`,
+			ratingAverage: sql`AVG(${postRatings.rating})`
+		})
+		.from(posts)
+		.leftJoin(postRatings, eq(postRatings.postId, posts.id))
+		.groupBy(posts.id)
+		.orderBy(posts.createdAt)
+		.limit(15)
+		.offset(offset);
 
-    return userPosts.map(r => ({
-        id: r.post.id,
-        creatorUserId: r.post.userId,
-        title: r.post.title,
-        content: r.post.content,
-        createdAt: r.post.createdAt.getTime(),
-        rating: {
-            amount: Number(r.ratingAmount ?? 0),
-            average: r.ratingAverage ? Number(r.ratingAverage) : 0
-        }
-    })) as PostDTO[];
+	return userPosts.map((r) => ({
+		id: r.post.id,
+		creatorUserId: r.post.userId,
+		title: r.post.title,
+		content: r.post.content,
+		createdAt: r.post.createdAt.getTime(),
+		rating: {
+			amount: Number(r.ratingAmount ?? 0),
+			average: r.ratingAverage ? Number(r.ratingAverage) : 0
+		}
+	})) as PostDTO[];
 });
 
 export const loadPostById = query(v.object({ id: v.number() }), async (schema) => {
-    const { id } = schema;
-    const userPosts = await db
-        .select({
-            post: posts,
-            ratingAmount: sql`COUNT(${postRatings.id})`,
-            ratingAverage: sql`AVG(${postRatings.rating})`
-        })
-        .from(posts)
-        .leftJoin(postRatings, eq(postRatings.postId, posts.id))
-        .where(eq(posts.id, id))
-        .groupBy(posts.id);
+	const { id } = schema;
+	const userPosts = await db
+		.select({
+			post: posts,
+			ratingAmount: sql`COUNT(${postRatings.id})`,
+			ratingAverage: sql`AVG(${postRatings.rating})`
+		})
+		.from(posts)
+		.leftJoin(postRatings, eq(postRatings.postId, posts.id))
+		.where(eq(posts.id, id))
+		.groupBy(posts.id);
 
-    return userPosts.map(r => ({
-        id: r.post.id,
-        creatorUserId: r.post.userId,
-        title: r.post.title,
-        content: r.post.content,
-        createdAt: r.post.createdAt.getTime(),
-        rating: {
-            amount: Number(r.ratingAmount ?? 0),
-            average: r.ratingAverage ? Number(r.ratingAverage) : 0
-        }
-    })) as PostDTO[];
+	return userPosts.map((r) => ({
+		id: r.post.id,
+		creatorUserId: r.post.userId,
+		title: r.post.title,
+		content: r.post.content,
+		createdAt: r.post.createdAt.getTime(),
+		rating: {
+			amount: Number(r.ratingAmount ?? 0),
+			average: r.ratingAverage ? Number(r.ratingAverage) : 0
+		}
+	})) as PostDTO[];
 });
 
 export const loadPostsByUserId = query(v.object({ userId: v.string() }), async (schema) => {
-    const { userId } = schema;
-    const userPosts = await db
-        .select({
-            post: posts,
-            ratingAmount: sql`COUNT(${postRatings.id})`,
-            ratingAverage: sql`AVG(${postRatings.rating})`
-        })
-        .from(posts)
-        .leftJoin(postRatings, eq(postRatings.postId, posts.id))
-        .where(eq(posts.userId, userId))
-        .groupBy(posts.id)
-        .orderBy(posts.createdAt);
+	const { userId } = schema;
+	const userPosts = await db
+		.select({
+			post: posts,
+			ratingAmount: sql`COUNT(${postRatings.id})`,
+			ratingAverage: sql`AVG(${postRatings.rating})`
+		})
+		.from(posts)
+		.leftJoin(postRatings, eq(postRatings.postId, posts.id))
+		.where(eq(posts.userId, userId))
+		.groupBy(posts.id)
+		.orderBy(posts.createdAt);
 
-    return userPosts.map(r => ({
-        id: r.post.id,
-        creatorUserId: r.post.userId,
-        title: r.post.title,
-        content: r.post.content,
-        createdAt: r.post.createdAt.getTime(),
-        rating: {
-            amount: Number(r.ratingAmount ?? 0),
-            average: r.ratingAverage ? Number(r.ratingAverage) : 0
-        }
-    })) as PostDTO[];
+	return userPosts.map((r) => ({
+		id: r.post.id,
+		creatorUserId: r.post.userId,
+		title: r.post.title,
+		content: r.post.content,
+		createdAt: r.post.createdAt.getTime(),
+		rating: {
+			amount: Number(r.ratingAmount ?? 0),
+			average: r.ratingAverage ? Number(r.ratingAverage) : 0
+		}
+	})) as PostDTO[];
 });
 
-/* TODO:
-export async function createPost(
-    event: RequestEvent,
-    data: { title: string; content: string }
-) {
-    const user = event.locals.user;
-    if (!user) throw error(401, "Unauthorized");
+export const createPost = form(
+	v.object({
+		title: v.pipe(v.string(), v.maxLength(256), v.nonEmpty()),
+		content: v.pipe(v.string(), v.maxLength(2056), v.nonEmpty()),
+		image: v.pipe(v.file(), v.mimeType(["image/png", "image/jpg", "image/jpeg"]))
+	}),
+	async ({ title, content, image }) => {
+		const event = getRequestEvent();
+		const user = event.locals.user;
+		if (!user) error(401, "Unauthorized");
 
-    const [newPost] = await db
-        .insert(posts)
-        .values({
-            userId: user.id,
-            title: data.title,
-            content: data.content
-        })
-        .returning();
+		const imageBuffer = await sharp(await image.arrayBuffer(), {
+			animated: true
+		})
+			.webp({})
+			.rotate()
+			.toBuffer();
 
-    return newPost;
-}
-*/
+		const [newPost] = await db
+			.insert(posts)
+			.values({
+				userId: user.id,
+				title,
+				content
+			})
+			.returning();
+
+		await minioClient.putObject(MINIO_BUCKET_NAME, `${newPost.id}.webp`, imageBuffer, imageBuffer.length, {
+			"Content-Type": "image/webp"
+		});
+
+		return newPost;
+	}
+);
---
+++ src/routes/upload/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { createPost } from "../post.remote";
+
+
+</script>
+
+<form {...createPost} enctype="multipart/form-data">
+    <label>
+        <h2>Title</h2>
+        <input {...createPost.fields.title.as("text")}>
+    </label>
+
+    <label>
+        <h2>Content</h2>
+        <textarea {...createPost.fields.content.as("text")}></textarea>
+    </label>
+
+    <input {...createPost.fields.image.as("file")}>
+
+    <button>Post!</button>
+</form>
