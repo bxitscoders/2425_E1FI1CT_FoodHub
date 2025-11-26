eb90ac9aa99b900a25ed41ee52900cc81be8c0f1
Christian Knapp <christian.alex.knapp@icloud.com>
2025-11-26 08:47:27 +0100

    refactor: add average rating on postsummary

--- src/lib/components/posts/PostSummary.svelte
+++ src/lib/components/posts/PostSummary.svelte
@@ -3,6 +3,8 @@
 	import { loadUserById } from "../../../routes/user.remote";
 	import type { User } from "$lib/auth-client";
 	import { loadImageByPostId } from "../../../routes/post.remote";
+	import CircleSlash2 from "@lucide/svelte/icons/circle-slash-2";
+	import Star from "@lucide/svelte/icons/star";
 
 	let { post }: { post: PostDTO } = $props();
 	let postCreator = $state<User | null>(null);
@@ -32,28 +34,36 @@
 	};
 </script>
 
-<div class="flex flex-row items-start gap-x-2">
-	<!-- Avatar -->
-	<img src={postCreator?.image} height="40px" width="40px" class="rounded-full" alt="User Avatar" />
+<div class="flex flex-col p-2 gap-y-2">
+	<div class="flex flex-row items-start gap-x-2">
+		<!-- Avatar -->
+		<img src={postCreator?.image} height="50px" width="50px" class="shrink-0 rounded-full" alt="User Avatar" />
 
-	<!-- Post Content -->
-	<div class="flex flex-col">
-		<!-- Post Header -->
-		<div class="flex items-center gap-x-1">
-			<span class="font-bold">{postCreator?.name}</span>
-			<span class="text-gray-800">@{postCreator?.handle}</span>
-			<span class="text-gray-700">•</span>
-			<span class="text-gray-700">{formatDate(post.createdAt ?? Date.now())}</span>
-		</div>
+		<div class="flex flex-col">
+			<div>
+				<span class="font-bold">{postCreator?.name}</span>
+				<span class="text-gray-800">@{postCreator?.handle}</span>
+				<span class="text-gray-700">•</span>
+				<span class="text-gray-700">{formatDate(post.createdAt ?? Date.now())}</span>
+			</div>
 
-		<!-- Post Message -->
-		<div>
-			<p>{post.content}</p>
-			{#await loadImageByPostId({ postId: post.id }) then imageUrl}
-				{#if imageUrl}
-					<img src={imageUrl} height="150px" width="150px" class="mt-2 rounded-lg" />
-				{/if}
-			{/await}
+			<div>{post.title}</div>
 		</div>
+	</div>
+
+	<span>{post.content}</span>
+
+	{#await loadImageByPostId({ postId: post.id }) then imageUrl}
+		{#if imageUrl}
+			<img src={imageUrl} height="150px" width="150px" class="mt-2 rounded-lg" />
+		{/if}
+	{/await}
+
+	<div class="flex gap-x-2 mt-2">
+		<CircleSlash2 height={7} width={7} class="italic" />
+		<span class="text-gray-600">{post.rating.average}</span>
+		<Star class="fill-yellow-500" />
+
+		<span class="ml-3 text-gray-600 italic">({post.rating.amount} Rating{post.rating.amount === 1 ? "" : "s"})</span>
 	</div>
 </div>
