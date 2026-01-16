a3f47d220a4eb5284bcb0cee5f5d4e43f14c2e51
Christian Knapp <christian.alex.knapp@icloud.com>
2026-01-16 10:15:37 +0100

    refactor: removed test site

--- src/routes/test/+server.ts
+++
@@ -1,7 +0,0 @@
-import { minioClient, MINIO_BUCKET_NAME } from '$lib/server/minio';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async () => {
-    await minioClient.putObject(MINIO_BUCKET_NAME, "bier.txt", "Hello, World!");
-    return new Response();
-};
